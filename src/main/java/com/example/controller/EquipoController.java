d64c06f822128a1c6ff0382980e4cd10cb0d38be
46989075y <ricardhuelamo@gmail.com>
2016-11-18 17:57:51 +0100

    CONTROL ERROR ID equipo

--- src/main/java/com/example/controller/EquipoController.java
+++ src/main/java/com/example/controller/EquipoController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import com.example.controller.DTO.Localidad;
+import com.example.controller.Util.HeaderUtil;
 import com.example.domain.Equipo;
 import com.example.domain.Jugador;
 import com.example.domain.Posicion;
@@ -10,8 +11,11 @@ import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -54,5 +58,21 @@ public class EquipoController {
 
         System.out.println();
         return localidades.asMap();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Equipo> createEquipo (@RequestBody Equipo equipo) throws URISyntaxException {
+        if(equipo.getId()!= null){
+            return ResponseEntity.
+                    badRequest().
+                    headers(
+
+                            HeaderUtil.
+                                    createFailureAlert("equipo", "idexists", "A new player cannot already have an ID")).body(null);
+
+        }
+        Equipo result = equipoRepository.save(equipo);
+        return ResponseEntity.created(new URI("/equipos/"+ result.getId())).headers(HeaderUtil.createEntityCreationAlert("equipo", result.getId(). toString())) .body(result);
     }
 }
